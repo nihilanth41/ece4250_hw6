36d8fad87349352ddb0f78d0e1dc1aa44d2b7587
Zachary Rump <zrrm74@mail.missouri.edu>
2017-03-21 13:21:16 -0500

    Finish problem 1

--- p1.vhd
+++ p1.vhd
@@ -1,12 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 -- write a vhdl function to create the 2's complement of an n-bit vector. 
 -- call like: comlement(a, 4);
-function comlement(datavec: std_logic_vector, n: integer) std_logic_vector is 
-	-- declarations
-	variable result: std_logic_vector(n-1 downto 0) := (others => '0');
+
+entity comlement_tb is
+end comlement_tb;
+
+architecture comlement_tb_arch of comlement_tb is
+
+-- datavec is bit vector, n is size of bit vector (# of bits)
+  function comlement(datavec : std_logic_vector; n : integer) return std_logic_vector is 
+    variable result : std_logic_vector(n-1 downto 0) := (others => '0');
+  begin
+    result := std_logic_vector(unsigned(not(datavec)) + 1);
+    return result;
+  end comlement;
+  
+  signal result1 : std_logic_vector(3 downto 0) := (others => '0');
+  signal result2 : std_logic_vector(7 downto 0) := (others => '0');
 begin
-	-- sequential statements
-	-- invert datavec, cast to unsigned
-	-- add 1, cast to std_logic_vector
-	result := std_logic_vector(unsigned(not(datavec) + 1));
-	return result;
-end comlement;
+
+  result1 <= comlement("1101", 4);
+  result2 <= comlement("11010001", 8) after 500 ns;
+
+end comlement_tb_arch;
